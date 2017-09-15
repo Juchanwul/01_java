988708337410d887f264ce7a6863988b3df7d53d
Juchanwul <juchanwul@naver.com>
2017-09-15 16:34:57 +0900

    Haha Hoho

---
+++ JAVA/Work/JaveApplication_test/src/main/java/java02/jv02_09_문자열겹합.java
@@ -0,0 +1,26 @@
+package java02;
+
+public class jv02_09_문자열겹합 {
+    
+    public static void main( String [] args) {
+        
+        int i = 10, j=20 ;
+        int sum = i + j ;
+        
+        
+        System.out.println("더하기:" + i + "+" + j + "=" + sum) ;
+        
+        String s1 = "Hello" ;
+        String s2 = " World" ;
+        System.out.println(s1 + s2) ;
+        
+        String s3 = s1 + s2 ;
+        System.out.println(s3) ;
+        
+        char a1 = '\u1121' ;
+        System.out.println(a1) ;
+       
+                
+    }
+    
+}
---
+++ JAVA/Work/JaveApplication_test/src/main/java/java02/jv02_10_산술연산과정.java
@@ -0,0 +1,23 @@
+package java02;
+
+public class jv02_10_산술연산과정 {
+    
+    public static void main (String [] args) {
+        
+        int x  ;
+        int y  ;
+        int sum ;
+
+        x = 2 ;
+        y = 3 ;
+        
+
+        
+        sum = x + y ;
+        
+        System.out.println(sum);
+        
+        
+    }
+    
+}
