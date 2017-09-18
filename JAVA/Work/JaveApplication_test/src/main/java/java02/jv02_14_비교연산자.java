644f80c9fa91717e965f9836eaefd6b413727c8a
Juchanwul <juchanwul@naver.com>
2017-09-18 12:00:31 +0900

    haha

--- JAVA/Work/JaveApplication_test/src/main/java/java02/jv02_14_비교연산자.java
+++ JAVA/Work/JaveApplication_test/src/main/java/java02/jv02_14_비교연산자.java
@@ -1,11 +1,12 @@
 package java02;
 
+import java.util.Scanner;
 
 public class jv02_14_비교연산자 {
     
     public static void main(String [] args) {
         
-        Scanner keyboard = new Scanner(System.in);
+        Scanner keyboard = new Scanner (System.in);
         
         // 정수 변수 선언 & 초기화
         int x = 3 ;
