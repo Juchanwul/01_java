84c7762be9b41d6e18ea6ecc366576c5e0093b87
Juchanwul <juchanwul@naver.com>
2017-09-18 18:03:30 +0900

    haha

--- JAVA/Work/JaveApplication_test/src/main/java/java03/ex03_01_LargeSmall.java
+++ JAVA/Work/JaveApplication_test/src/main/java/java03/ex03_01_LargeSmall.java
@@ -6,13 +6,15 @@ public class ex03_01_LargeSmall {
     
     public static void main (String [] args) {
         
+        int n = 0 ;
+        
         Scanner keyboard = new Scanner(System.in);
         System.out.println("숫자를 입력하세요.");
-        int n = keyboard.nextInt();
+        n = keyboard.nextInt();
         
         keyboard.close();
         
-        if ( n>= 100) {
+        if ( n >= 100) {
             System.out.println("large");
         }
         
---
+++ JAVA/Work/JaveApplication_test/src/main/java/java03/jv03_03_중첩ifelse.java
@@ -0,0 +1,20 @@
+package java03;
+
+import java.util.Scanner;
+
+public class jv03_03_중첩ifelse {
+    
+    public static void main (String [] args) {
+        
+        int grade = 0 ;
+        
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Grade : ");
+        grade = keyboard.nextInt();
+        
+        System.out.println("학점 : " + grade);
+        
+
+    }
+    
+}
