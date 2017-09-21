2674b4efbcf08dfe5c174a5bfa127dfeb21c41f8
Juchanwul <juchanwul@naver.com>
2017-09-21 18:13:05 +0900

    haha

---
+++ JAVA/Work/JaveApplication_test/src/main/java/java03/ex03_10_LowerUpper.java
@@ -0,0 +1,19 @@
+package java03;
+
+import java.util.Scanner;
+
+public class ex03_10_LowerUpper {
+    
+    public static void main (String[]args){
+        
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("ch : " );
+        int c = Integer.toString();
+        
+        
+        
+        if (c =  )
+       
+    }
+    
+}
