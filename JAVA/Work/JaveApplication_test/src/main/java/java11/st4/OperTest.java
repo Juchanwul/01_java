2a7751635e462922b26a763552f1099827c5ffc6
Juchanwul <juchanwul@naver.com>
2017-10-12 14:34:03 +0900

    haha

--- JAVA/Work/JaveApplication_test/src/main/java/java11/st4/OperTest.java
+++ JAVA/Work/JaveApplication_test/src/main/java/java11/st4/OperTest.java
@@ -5,9 +5,7 @@ import java.util.Scanner;
 public class OperTest {
     
     public static void main(String[] args) {
-        int n1 = 0;
-        int n2 = 0;
-        
+      
         Oper myOper = new Oper();
         Scanner keyboard = new Scanner(System.in);
         System.out.print("First num : ");
